702b8f4297c19bf0c1abe1800d10bbef8e073b5d
Ajith Sritharan <sritharanajith04@gmail.com>
2024-09-29 07:31:57 +0530

    initial

---
+++ src/app/Components/more-to-love/more-to-love.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-to-love',
+  standalone: true,
+  imports: [],
+  templateUrl: './more-to-love.component.html',
+  styleUrl: './more-to-love.component.css'
+})
+export class MoreToLoveComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { BannerComponent } from './Components/banner/banner.component';
+import { NewProductsComponent } from './Components/new-products/new-products.component';
+import { MoreToLoveComponent } from './Components/more-to-love/more-to-love.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, BannerComponent, NewProductsComponent, MoreToLoveComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
